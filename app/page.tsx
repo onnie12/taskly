6b54f9b03306f90ee6b97aec96d1f078716016af
Onni <152686116+onnie12@users.noreply.github.com>
2025-03-25 19:24:52 +0100

    postgres added

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import Navbar from './components/Navbar'
 import Description from './components/Description'
-import MainFooter from './components/MainFooter'
 import Background from './components/Background'
 function Home() {
   return(
     <>
     <Navbar></Navbar>
     <Description></Description>
-    <MainFooter></MainFooter>
     <Background></Background>
     </>
   )
